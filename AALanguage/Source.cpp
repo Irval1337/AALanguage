3b24399bfaa3d8ccd528df67f32bcfbb14ed897c
Irval <56792892+Irval1337@users.noreply.github.com>
2023-11-26 21:42:46 +0300

    Minor fixes

--- AALanguage/Source.cpp
+++ AALanguage/Source.cpp
@@ -24,10 +24,10 @@ bool is_identifier(std::string s) {
 literal_type is_literal(std::string s) {
 	if (s == "true" || s == "false") return literal_type::logical;
 
-	static const std::regex integer_r(R"([0-9]{1,}(ULL|ull|LL|ll|I|i|U|u)?)");
+	static const std::regex integer_r(R"([0-9]{1,}(UL|ul|L|l|I|i|U|u|s|S)?)");
 	if (std::regex_match(s, integer_r)) return literal_type::integer;
 
-	static const std::regex real_r(R"(([0-9]{1,}.[0-9]{1,})(e(\+|-)?[0-9]{1,})?(d|D|f|F)?)");
+	static const std::regex real_r(R"(([0-9]{1,}.[0-9]*)(e(\+|-)?[0-9]{1,})?(d|D|f|F)?)");
 	if (std::regex_match(s, real_r)) return literal_type::real;
 
 	static const std::regex symbol_r(R"(\'(.|\\n|\\t|\\0)\')");
@@ -40,7 +40,7 @@ literal_type is_literal(std::string s) {
 }
 
 bool is_operation(std::string s) {
-	return s == "[]" || s == "." || s == "++" || s == "--" || s == "+" || s == "-" || s == "!" || s == "~" || s == "*" || s == "/" || s == "%" ||
+	return s == "[" || s == "]" || s == "." || s == "++" || s == "--" || s == "+" || s == "-" || s == "!" || s == "~" || s == "*" || s == "/" || s == "%" ||
 		s == ">>" || s == "<<" || s == ">" || s == ">=" || s == "<" || s == "<=" || s == "==" || s == "!=" || s == "&" || s == "^" || s == "|" ||
 		s == "&&" || s == "**" || s == "||" || s == "=" || s == "+=" || s == "-=" || s == "*=" || s == "/=" || s == "%=";
 }
@@ -63,6 +63,7 @@ bool is_comment(std::string s) {
 }
 
 token_type get_token_type(Trie* service_trie, std::string s) {
+	if (is_comment(s)) return token_type::comment;
 	if (is_service(service_trie, s)) return token_type::service;
 	if (is_identifier(s)) return token_type::identifier;
 	if (is_literal(s) != literal_type::none) return token_type::literal;
@@ -70,7 +71,6 @@ token_type get_token_type(Trie* service_trie, std::string s) {
 	if (is_punctuation(s)) return token_type::punctuation;
 	if (is_comma(s)) return token_type::comma;
 	if (is_brace(s)) return token_type::brace;
-	if (is_comment(s)) return token_type::comment;
 	return token_type::unknown;
 }
 
@@ -99,6 +99,8 @@ void print_token(Trie* service_trie, std::string buffer) {
 	case brace:
 		std::cout << "(brace, \"" << buffer << "\")\n";
 		break;
+	case comment:
+		break;
 	case unknown:
 		std::cout << "(unknown, \"" << buffer << "\")\n";
 		exit(0);
@@ -125,8 +127,10 @@ int main() {
 	std::string buffer = "";
 	char c;
 	token_type prev = token_type::unknown;
+	bool prev_quotation = false, prev_single = false;
 	while (file >> std::noskipws >> c) {
 		if (c == ' ' || c == '\n') {
+			if (prev == token_type::comment && c == ' ') continue;
 			print_token(service_trie, buffer);
 			prev = token_type::unknown;
 			buffer.clear();
@@ -148,6 +152,16 @@ int main() {
 				prev = get_token_type(service_trie, buffer);
 				buffer = std::string(1, c);
 			} else {
+				if (c == '"') {
+					if (!prev_quotation) {
+						prev_quotation = true;
+					}
+				} else if (c == '\'') {
+					if (!prev_single) {
+						prev_single = true;
+					}
+				}
+				if (prev_quotation || prev_single) continue;
 				print_token(service_trie, buffer);
 				exit(0);
 			}
